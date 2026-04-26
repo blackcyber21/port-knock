7f53dec7a3abfed1dc2f67fd7a01d102ab979d9d
Rafael Costa <35323386+blackcyber21@users.noreply.github.com>
2026-04-26 00:26:25 +0000

    Replace hardcoded IP with variable in undoknock.sh

--- undoknock.sh
+++ undoknock.sh
@@ -1,5 +1,5 @@
 ip=172.16.1.55
 #hping3 -S -c 1 -p 2424 37.59.174.235 >/dev/null
 #hping3 -S -c 1 -p 2525 37.59.174.235 >/dev/null
-hping3 -S -c 1 -p 7990 172.16.1.55 >/dev/null
-hping3 -S -c 1 -p 3443 172.16.1.55 >/dev/null
+hping3 -S -c 1 -p 7990 $ip >/dev/null
+hping3 -S -c 1 -p 3443 $ip >/dev/null
